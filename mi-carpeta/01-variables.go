e11429e6d424950f53657cb84dbf6e21087f13d8
Wize6669 <55813562+Wize669@users.noreplying.github.com>
2019-10-19 12:50:42 -0500

    aumentando

--- mi-carpeta/01-variables.go
+++ mi-carpeta/01-variables.go
@@ -16,6 +16,38 @@ comple(x64 y x128) --- Números complejos con float32/64 partes reales e imagina
 Boolean – V o F
 String – cadena de caracteres /arreglos
 
+los siguiente describe quien son imnutables
+
+immutable fields
+immutable methods
+immutable arguments
+immutable return values
+immutable variables
+immutable interfaces
+immutable reference types and containers
+immutable package-scope variables
+
+leer
+
+Mutable Go objects:
+
+arrays and slices
+maps
+channels
+closures which are capturing at least 1 variable from the outer scope
+
+Immutable Go objects:
+
+interfaces
+booleans, numeric values (including values of type int)
+strings
+pointers
+function pointers, and closures which can be reduced to function pointers
+structs having a single field
+Go objects which some people may consider mutable, while other people may consider them immutable:
+
+structs having multiple fields
+
 */
 
 func main() {
