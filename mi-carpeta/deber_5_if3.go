2d6dc9dabd7d35b62d77ae05a9cfd889c3f782b9
Wize6669 <55813562+Wize669@users.noreplying.github.com>
2019-11-06 13:20:35 -0500

    Arreglo

--- mi-carpeta/deber_5_if3.go
+++ mi-carpeta/deber_5_if3.go
@@ -1,49 +1,48 @@
 // Saber si ya estas hecho papel higenico (estas ebrio)
 package main
+
 import (
 	"fmt"
 )
 
-func main(){
-	fmt.Println("\nSaber si ya estas hecho papel higenico\n")
+func main() {
+	fmt.Println("Saber si ya estas hecho papel higenico")
 	fmt.Println("Ingrese el numero de cervezas que hay bebido: ")
-var nuvi,resp int
-fmt.Scanln(&nuvi)
-if nuvi>=1 && nuvi<=3 {
-	fmt.Println("Todavia jalas !!")
-} 
-else if nuvi >3 && nuvi <= 5 {
-	fmt.Println("(Si 1/ No 0) || Quiere llamar a su ex ¿?")
-	fmt.Scanln(&resp)
-	if resp !=1 && resp !=0{
-		fmt.Println("Revise su respuesta !!")
-	} else if resp == 1{
-		fmt.Println("Que pendej@ anda a ver mejor master chef")
-	} else  {
-		fmt.Println("Muy bien")
+	var nuvi, resp int
+	fmt.Scanln(&nuvi)
+	if nuvi >= 1 && nuvi <= 3 {
+		fmt.Println("Todavia jalas !!")
+	} else if nuvi > 3 && nuvi <= 5 {
+		fmt.Println("(Si 1/ No 0) / Quiere llamar a su ex ")
+		fmt.Scanln(&resp)
+		if resp != 1 && resp != 0 {
+			fmt.Println("Revise su respuesta !!")
+		} else if resp == 1 {
+			fmt.Println("Que pendej@ anda a ver mejor master chef")
+		} else {
+			fmt.Println("Muy bien")
+		}
+		fmt.Println("(Si 1/ No 0) / Puede hablar ")
+		fmt.Scanln(&resp)
+		if resp != 1 && resp != 0 {
+			fmt.Println("Revise su respuesta !!")
+		} else if resp == 1 {
+			fmt.Println("Una cerveza más")
+		} else {
+			fmt.Println("Dile a alguien que te de pidiendo un taxi/uber y andate a dormir")
+		}
+		fmt.Println("(Si 1/ No 0)  Puede caminar ")
+		fmt.Scanln(&resp)
+		if resp != 1 && resp != 0 {
+			fmt.Println("Revise su respuesta !!")
+		} else if resp == 1 {
+			fmt.Println("Una cerveza más")
+		} else {
+			fmt.Println("Dile a alguien que te de pidiendo un taxi/uber y andate a dormir")
+		}
+
+	} else if nuvi > 5 {
+		fmt.Println("Mejor pide taxi/uber y andate a dormir  ")
 	}
-
-	fmt.Println("(Si 1/ No 0) ||Puede hablar ¿?")
-	fmt.Scanln(&resp)
-	if resp !=1 && resp !=0{
-		fmt.Println("Revise su respuesta !!")
-	} else if resp == 1{
-		fmt.Println("Una cerveza más")
-	} else {
-		fmt.Println("Dile a alguien que te de pidiendo un taxi/uber y andate a dormir")}
-
-	fmt.Println("(Si 1/ No 0) ||Puede caminar ¿?")
-	fmt.Scanln(&resp)
-	if resp !=1 && resp !=0{
-		fmt.Println("Revise su respuesta !!")
-	} else if resp == 1{
-		fmt.Println("Una cerveza más")
-	} else  {
-		fmt.Println("Dile a alguien que te de pidiendo un taxi/uber y andate a dormir")
-
-
-} else if nuvi > 5 {
-	fmt.Println("Mejor pide taxi/uber y andate a dormir ")
-}
 
 }
