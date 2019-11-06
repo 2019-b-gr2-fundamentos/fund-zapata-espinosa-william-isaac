7730eb9f7620faa56a344a0101c210a1b4a1781d
William <william.zapata@epn.edu.ec>
2019-11-06 17:07:52 -0500

    cla

---
+++ mi-carpeta/iterables/01_while.js
@@ -0,0 +1,50 @@
+// do while
+var vecesQueHeComido = 0;
+console.log("Vamos a comer");
+do {
+    console.log("Vamos a comer");
+    vecesQueHeComido = vecesQueHeComido + 1;
+    switch (vecesQueHeComido) {
+        case 1:
+            console.log("Desayuno");
+            break;
+        case 2:
+            console.log("Almuerzo");
+            break;
+        case 3:
+            console.log("Merienda");
+            break;
+        default:
+            console.log("Gorditos");
+            break;
+    }
+} while (vecesQueHeComido < 3);
+// while
+var vecesQueHeComido2 = 0;
+while (vecesQueHeComido2 < 3) {
+    console.log("Vamos a comer");
+    vecesQueHeComido2 = vecesQueHeComido2 + 1;
+    switch (vecesQueHeComido2) {
+        case 1:
+            console.log("Desayuno");
+            break;
+        case 2:
+            console.log("Almuerzo");
+            break;
+        case 3:
+            console.log("Merienda");
+            break;
+        default:
+            console.log("Gorditos");
+            break;
+    }
+}
+/*
+1 Analisis
+2 Fundamentos
+3 Calculo
+5 Fisic
+11 Ingles
+
+
+*/ 
---
+++ mi-carpeta/iterables/01_while.ts
@@ -0,0 +1,52 @@
+// do while
+let vecesQueHeComido = 0;
+console.log("Vamos a comer")
+do { 
+    console.log("Vamos a comer")
+    vecesQueHeComido = vecesQueHeComido +1;
+    switch(vecesQueHeComido){
+        case 1:
+            console.log("Desayuno")
+            break;
+        case 2:
+            console.log("Almuerzo")
+             break;
+        case 3:
+            console.log("Merienda")
+            break;
+      default:
+            console.log("Gorditos")
+            break;
+    }
+} while(vecesQueHeComido < 3)
+
+// while
+
+let vecesQueHeComido2 = 0;
+while(vecesQueHeComido2 < 3){
+    console.log("Vamos a comer")
+    vecesQueHeComido2 = vecesQueHeComido2 +1;
+    switch(vecesQueHeComido2){
+        case 1:
+            console.log("Desayuno")
+            break;
+        case 2:
+            console.log("Almuerzo")
+             break;
+        case 3:
+            console.log("Merienda")
+            break;
+      default:
+            console.log("Gorditos")
+            break;
+    }
+}
+/* 
+1 Analisis
+2 Fundamentos
+3 Calculo
+5 Fisic
+11 Ingles
+
+
+*/
