bf6382223fc3c1e3679c7e3843447f64a371f6cd
Wize6669 <55813562+Wize669@users.noreplying.github.com>
2019-11-27 22:21:55 -0500

    Correcion

--- mi-carpeta/Funciones/recursibas.js
+++ mi-carpeta/Funciones/recursibas.js
@@ -11,18 +11,44 @@ function imprimirMensajeNvECES(mensaje, numeroVences) {
 //function main(){
 //imprimirMensajeNvECES('holas',3);
 //}
-var arregloa = [1, 2, 3, 4, 5];
-console.log(arregloa);
-function imprimirElemenrtosDeUnArreglo(arregloa, indice) {
+//const arregloa = [1,2,3,4,5];
+//console.log(arregloa);
+//function imprimirElemenrtosDeUnArreglo(arregloa: Number[], indice:number):void{
+//if(indice <= 4){
+//  console.log(arregloa[indice]);
+//  const indiceNuevos = indice + 1;
+//  imprimirElemenrtosDeUnArreglo(arregloa, indiceNuevos);
+//}else {
+// console.log("Se ha mostrado todos los elementos XD")
+//}
+//}
+//function main(){
+//imprimirElemenrtosDeUnArreglo(arregloa,0);
+//}
+var arreglob = [];
+var arreglo = [1, 2, 3, 4, 5];
+console.log(arreglo);
+function elementosDeArreglo(arreglo, indice) {
     if (indice <= 4) {
-        console.log(arregloa[indice]);
-        var indiceNuevos = indice + 1;
-        imprimirElemenrtosDeUnArreglo(arregloa, indiceNuevos);
+        console.log(arreglo[indice]);
+        var indiceNuevosa_1 = indice + 1;
+        elementosDeArreglo(arreglo, indiceNuevosa_1);
     }
     else {
-        console.log("Se ha mostrado todos los elementos XD");
+        console.log("El arreglo es: ");
+    }
+    console.log("El arreglo invertido es: ");
+    var indiceNuevosa = 4;
+    if (indiceNuevosa >= 0) {
+        console.log(arreglo[indice]);
+        var indiceNuevosb = indice - 1;
+        elementosDeArreglo(arreglo, indiceNuevosb);
+    }
+    else {
+        console.log("GRACIAS");
     }
 }
 function main() {
-    imprimirElemenrtosDeUnArreglo(arregloa, 0);
+    elementosDeArreglo(arreglo, 0);
+    elementosDeArreglo(arreglob, 4);
 }
--- mi-carpeta/Funciones/recursibas.ts
+++ mi-carpeta/Funciones/recursibas.ts
@@ -16,20 +16,47 @@ if(numeroVences == 0){
 //}
 
 
-const arregloa = [1,2,3,4,5];
-console.log(arregloa);
+//const arregloa = [1,2,3,4,5];
+//console.log(arregloa);
 
-function imprimirElemenrtosDeUnArreglo(arregloa: Number[], indice:number):void{
-if(indice <= 4){
-    console.log(arregloa[indice]);
-    const indiceNuevos = indice + 1;
-    imprimirElemenrtosDeUnArreglo(arregloa, indiceNuevos);
-}else {
-    console.log("Se ha mostrado todos los elementos XD")
+//function imprimirElemenrtosDeUnArreglo(arregloa: Number[], indice:number):void{
+//if(indice <= 4){
+  //  console.log(arregloa[indice]);
+  //  const indiceNuevos = indice + 1;
+  //  imprimirElemenrtosDeUnArreglo(arregloa, indiceNuevos);
+//}else {
+   // console.log("Se ha mostrado todos los elementos XD")
+//}
+
+//}
+
+//function main(){
+    //imprimirElemenrtosDeUnArreglo(arregloa,0);
+//}
+
+const arreglob = [];
+const arreglo = [1,2,3,4,5]; 
+console.log(arreglo);
+function elementosDeArreglo(arreglo:number[], indice:number):void{
+if(indice <=4){
+    console.log(arreglo[indice]);
+    const indiceNuevosa = indice + 1;
+    elementosDeArreglo(arreglo, indiceNuevosa);
+}else{
+    console.log("El arreglo es: ");
+}
+console.log("El arreglo invertido es: ");
+const indiceNuevosa = 4;
+if(indiceNuevosa >=0){
+console.log(arreglo[indice]);
+const indiceNuevosb = indice - 1;
+elementosDeArreglo(arreglo, indiceNuevosb);
+}else{
+    console.log("GRACIAS");
 }
 
 }
-
 function main(){
-    imprimirElemenrtosDeUnArreglo(arregloa,0);
+    elementosDeArreglo(arreglo,0);
+    elementosDeArreglo(arreglob,4);
 }
