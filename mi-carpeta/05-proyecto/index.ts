b04e522a3ff36f8b459656064cbe29cb03f80ab5
William <william.zapata@epn.edu.ec>
2020-01-06 16:04:21 -0500

    Libre

---
+++ mi-carpeta/05-proyecto/index.ts
@@ -0,0 +1,8 @@
+// NODE MODULE
+// const sumaLibreria = require('william-zapata/lib/suma');
+/*
+Importar todo el contenido con el nombre 'sumaLibreria' del paquete 'william-zapata'
+*/
+import * as sumaLibreria from 'william-zapata/lib/suma'
+const respuesta = sumaLibreria(1,2);
+console.log('La respuesta es: ',respuesta);
