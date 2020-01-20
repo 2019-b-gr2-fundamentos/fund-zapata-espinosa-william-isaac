f6bba65fa7b59037e5792fdd88c2a0bd2ba95346
William <william.zapata@epn.edu.ec>
2020-01-20 15:46:03 -0500

    Class

--- mi-carpeta/06-estructuraDeDatos/interfaces/02-crud.ts
+++ mi-carpeta/06-estructuraDeDatos/interfaces/02-crud.ts
@@ -1,9 +1,15 @@
 // 02 crud.ts create read update delete
 // const prompts = require('prompts');
 import * as prompts from 'prompts';
+import { RespuestaEdad } from './respuesta-edad.interface';
+import { RespuestaNombre } from './respuesta-nombre.interface';
+import { RespuestaCedula } from './respuesta-cedula.interface';
 
 function main(){
-    obtenerDatosAnimalPerrito();
+    // obtenerDatosAnimalPerrito(); // Asincrona
+    obtenerDatosAnimalPerritoSincrona()
+    .then()
+    .catch(); // -> Promesa // Sincrona
 
 }
 
@@ -43,8 +49,51 @@ function obtenerDatosAnimalPerrito(){
         
         }
     )
-    console.log(promesaEdad)
     console.log('Fin')
 
 }
+
+async function obtenerDatosAnimalPerritoSincrona(){
+    /*
+    Paradigma de programacion 
+    SINCRONO VS ASINCRONA
+   */
+    console.log('Inicio ')
+    const preguntas = [
+        {
+            type: 'number',
+            name: 'edad',
+            message: 'Puedes darme tu edad?'
+        },
+        {
+            type: 'text',
+            name: 'nombre',
+            message: 'Puedes darme tu nombre?'
+        },
+        {
+            type: 'text',
+            name: 'cedula',
+            message: 'Puedes darme tu cedula?'
+        }
+    ]
+    //const edad: RespuestaEdad = await prompts({  // PROMESA
+       // type: 'number',
+       // name: 'edad',
+       // message: 'Puedes darme tu edad?'
+    //});
+    //const nombre: RespuestaNombre = await prompts({  // PROMESA
+        //type: 'text',
+       // name: 'nombre',
+        //message: 'Puedes darme tu nombre?'
+    //});
+    //const cedula: RespuestaCedula = await prompts({  // PROMESA
+      //  type: 'number',
+      //  name: 'cedula',
+      //  message: 'Puedes darme tu cedula?'
+    //});
+    const respuestaEdad = await prompts(preguntas);
+    console.log('Respuesta', respuestaEdad);
+    console.log('Fin');
+}
+
 main();
